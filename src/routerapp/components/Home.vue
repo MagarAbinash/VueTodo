b6d48976254d290fddca6a9533fb55394880aa9f
Abinash magar <magarabinash986@gmail.com>
2021-02-10 14:58:22 +0545

    Json-Server added

--- src/routerapp/components/Home.vue
+++ src/routerapp/components/Home.vue
@@ -1,5 +1,10 @@
 <template>
   <h1>Home</h1>
+  <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+    Quaerat quis reiciendis obcaecati sed dicta iure error doloribus impedit dignissimos, 
+    asperiores illo rerum neque inventore consequuntur voluptas exercitationem repellendus, 
+    dolorum nam!
+  </p>
 </template>
 
 <script>
